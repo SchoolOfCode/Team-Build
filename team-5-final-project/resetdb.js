5c8564bedd70616cba9d3bf6f9cf7eb025d2d3bd
wendyrich63 <142948201+wendyrich63@users.noreply.github.com>
2023-12-05 16:40:13 +0000

    set up test supabase queries not yet working

--- team-5-final-project/resetdb.js
+++ team-5-final-project/resetdb.js
@@ -1,31 +1,16 @@
-import Navbar from "../components/navbar";
-
 import {v4 as uuidv4} from "uuid";
 import {supabase} from "../../../supabase.js";
-console.log(supabase);
+
+async function resetDB(){
 
 
 //sample READ all - read all data from the recipes table. Returns an array of objects
-const { data: result2, error2 } = await supabase.from('charity_user_pref').select();
-console.log(result2);
-console.log(error2);
-const display2 = result2[0];
+// const { data: result2, error2 } = await supabase.from("recipes").select();
+// const display2 = result2[0];
 
 
 //sample INSEET - updates the name field in the recipes table with the new value 'Rice K'. ONLY returns an error
-console.log("hello");
 const {  error3 } = await supabase.from("charity_user_pref").insert({id: uuidv4(), first_name: 'David', surname: 'Dixon', contact_number: 676534251, org_name: 'our first charity', charity_reg_no: 7656435, t_and_c: 'true'});
-  
 
-
-
-export default function About() {
- 
-  return (
-    <div>
-      <Navbar />
-      <h1>About Us Landing Page</h1>
-      <p>{display2.surname}</p>
-    </div>
-  );
 }
+await resetDB();
--- team-5-final-project/supabase.js
+++ team-5-final-project/supabase.js
@@ -5,3 +5,4 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+

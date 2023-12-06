51bf5f7bc24ecf98ded1acb7f24e11256d68924a
davidjohnd <99533641+davidjohnd@users.noreply.github.com>
2023-12-06 11:11:12 +0000

    post test working with database

--- team-5-final-project/src/app/about/page.jsx
+++ team-5-final-project/src/app/about/page.jsx
@@ -2,8 +2,12 @@ import Navbar from "../components/navbar";
 
 import {v4 as uuidv4} from "uuid";
 import {supabase} from "../../../supabase.js";
-console.log(supabase);
+// console.log(supabase);
 
+//sample INSEET - updates the name field in the recipes table with the new value 'Rice K'. ONLY returns an error
+console.log("hello");
+const {  error3 } = await supabase.from('charity_user_pref').insert({id: 'bd688fda-9486-4c46-9651-89afb301fe91', first_name: 'David', surname: 'Dixon', contact_number: '0676534251', org_name: 'our first charity', charity_reg_no: '7656435', t_and_c: true});
+  
 
 //sample READ all - read all data from the recipes table. Returns an array of objects
 const { data: result2, error2 } = await supabase.from('charity_user_pref').select();
@@ -12,10 +16,7 @@ console.log(error2);
 const display2 = result2[0];
 
 
-//sample INSEET - updates the name field in the recipes table with the new value 'Rice K'. ONLY returns an error
-console.log("hello");
-const {  error3 } = await supabase.from("charity_user_pref").insert({id: uuidv4(), first_name: 'David', surname: 'Dixon', contact_number: 676534251, org_name: 'our first charity', charity_reg_no: 7656435, t_and_c: 'true'});
-  
+
 
 
 
@@ -25,7 +26,7 @@ export default function About() {
     <div>
       <Navbar />
       <h1>About Us Landing Page</h1>
-      <p>{display2.surname}</p>
+      {/* <p>{display2.surname}</p> */}
     </div>
   );
 }

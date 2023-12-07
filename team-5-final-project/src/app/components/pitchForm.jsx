b6710b10780d32066753dbcf599d1fb9112d3697
Mahad Mohamed <mahad.mohamed1@outlook.com>
2023-12-07 14:00:32 +0000

    pitch forms persisting to db

--- team-5-final-project/src/app/components/pitchForm.jsx
+++ team-5-final-project/src/app/components/pitchForm.jsx
@@ -13,7 +13,6 @@ export default function PitchForm() {
 
   const [regSuccess, setRegSuccess] = useState(false);
   const [regSuccessMessage, setRegSuccessMessage] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
   const [isValid, setIsValid] = useState(true);
   const [submissionMessage, setSubmissionMessage] = useState("");
 
@@ -58,114 +57,114 @@ export default function PitchForm() {
       if (error) {
         console.error(error);
         return;
-      } else {
       }
 
       setRegSuccess(true);
       setRegSuccessMessage("Registration successful!");
       setSubmissionMessage("Your project has been submitted.");
     } catch (error) {
-      console.log(`Failed to add project to db ${error}`);
+      console.error(`Failed to add project to db ${error}`);
     }
   };
-}
-return (
-  <div className="min-h-screen flex pt-8 justify-left pl-5 border border-black rounded-2xl p-2 m-2 bg-amber-50">
-    <div className="">
-      {regSuccess ? (
-        <div>
-          {regSuccessMessage}
-          <div className="text-green-500">{submissionMessage}</div>
-        </div>
-      ) : (
-        <form
-          className=""
-          method="POST"
-          action="https://team-5-final-project-pi.vercel.app/developer/register"
-          onSubmit={submitReg}
-        >
-          <div className="">
-            <label className="block text-sm font-semibold mb-1 text-black">
-              Project Title:
-            </label>
-            <input
-              type="text"
-              name="Project_Title"
-              onChange={handleInput}
-              value={registration.Project_Title}
-              className="w-80 px-4 py-2 border border-gray-300 rounded"
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-semibold mb-1 text-black">
-              Brief Summary:
-            </label>
-            <input
-              type="text"
-              name="Short_Descr"
-              onChange={handleInput}
-              value={registration.Short_Descr}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-semibold mb-1 text-black">
-              Detailed Description:
-            </label>
-            <input
-              type="VARCHAR"
-              name="Long_Descr"
-              onChange={handleInput}
-              value={registration.Long_Descr}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md"
-              required
-            ></input>
-          </div>
 
+  return (
+    <div className="min-h-screen flex pt-8 justify-left pl-5 border border-black rounded-2xl p-2 m-2 bg-amber-50">
+      <div className="">
+        {regSuccess ? (
           <div>
-            <div>
+            {regSuccessMessage}
+            <div className="text-green-500">{submissionMessage}</div>
+          </div>
+        ) : (
+          <form
+            className=""
+            method="POST"
+            action="https://team-5-final-project-pi.vercel.app/developer/register"
+            onSubmit={submitReg}
+          >
+            <div className="">
               <label className="block text-sm font-semibold mb-1 text-black">
-                YouTube Video Link:
+                Project Title:
               </label>
               <input
-                name="Video_Link"
+                type="text"
+                name="Project_Title"
                 onChange={handleInput}
-                value={registration.Video_Link}
+                value={registration.Project_Title}
+                className="w-80 px-4 py-2 border border-gray-300 rounded"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-semibold mb-1 text-black">
+                Brief Summary:
+              </label>
+              <input
+                type="text"
+                name="Short_Descr"
+                onChange={handleInput}
+                value={registration.Short_Descr}
                 className="w-full px-4 py-2 border border-gray-300 rounded-md"
                 required
-              ></input>
+              />
             </div>
-          </div>
-
-          <div className="flex flex-wrap justify-between">
-            <label className="block text-sm font-semibold mb-1 text-black">
-              Terms & Conditions:
-            </label>
-            <input
-              type="checkbox"
-              name="t_and_c"
-              onChange={handleInput}
-              value={registration.t_and_c}
-              className="w-full px-4 py-2 border border-gray-300 rounded-md"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-md"
-          >
-            Submit Form
-          </button>
-
-          {!isValid && (
-            <div className="text-red-500">
-              Please fill in all required fields.
+            <div>
+              <label className="block text-sm font-semibold mb-1 text-black">
+                Detailed Description:
+              </label>
+              <input
+                type="VARCHAR"
+                name="Long_Descr"
+                onChange={handleInput}
+                value={registration.Long_Descr}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                required
+              />
             </div>
-          )}
-        </form>
-      )}
+
+            <div>
+              <div>
+                <label className="block text-sm font-semibold mb-1 text-black">
+                  YouTube Video Link:
+                </label>
+                <input
+                  name="Video_Link"
+                  onChange={handleInput}
+                  value={registration.Video_Link}
+                  className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-wrap justify-between">
+              <label className="block text-sm font-semibold mb-1 text-black">
+                Terms & Conditions:
+              </label>
+              <input
+                type="checkbox"
+                name="t_and_c"
+                onChange={handleInput}
+                value={registration.t_and_c}
+                className="w-full px-4 py-2 border border-gray-300 rounded-md"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 rounded-md"
+            >
+              Submit Form
+            </button>
+
+            {!isValid && (
+              <div className="text-red-500">
+                Please fill in all required fields.
+              </div>
+            )}
+          </form>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+}

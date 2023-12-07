ea90a13e3600d74c60d82563d9dddb11ae76d67e
wendyrich63 <142948201+wendyrich63@users.noreply.github.com>
2023-12-07 16:33:45 +0000

    starting the voting page - fetch products not working

--- team-5-final-project/src/app/developers/dashboard/voting/page.jsx
+++ team-5-final-project/src/app/developers/dashboard/voting/page.jsx
@@ -1,10 +1,17 @@
+"use client";
 import Navbar from "@/app/components/navbar";
+import DisplayProjectsForVoting from "@/app/components/votingDispPrjs";
+
+
+
+ 
 
 export default function Voting() {
   return (
     <div>
       <Navbar />
       <h1>Voting</h1>
+      <DisplayProjectsForVoting />
     </div>
   );
 }

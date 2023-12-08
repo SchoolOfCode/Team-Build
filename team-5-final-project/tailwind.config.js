a9db0c8e36e33a0898a6ac7d6aa5bb3ecbb6f970
Yogiz88 <m.z.zawadzkimichal@gmail.com>
2023-12-08 12:34:00 +0000

    font issues

--- team-5-final-project/tailwind.config.js
+++ team-5-final-project/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,17 +7,15 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    // colors: {
-    //   'navbar': '#66CB9D',
-    //   'divBackground': '#F5F5E5',
-    // },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        header: "Superc"
     },
   },
   plugins: [],
-}
+  }}

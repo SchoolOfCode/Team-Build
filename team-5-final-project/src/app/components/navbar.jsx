95aa8da473cce40ca198fce5ebf83621311d7ea8
Yogiz88 <m.z.zawadzkimichal@gmail.com>
2023-12-11 14:09:21 +0000

    landing page, navbar

--- team-5-final-project/src/app/components/navbar.jsx
+++ team-5-final-project/src/app/components/navbar.jsx
@@ -10,11 +10,11 @@ export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <div className="bg-yellow-100">
-      <nav className="w-full top-0 left-0 right-0">
-        <div className="justify-between mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+    <div className="bg-gray-200">
+      <nav className="w-full">
+        <div className="justify-between mx-auto md:items-center md:flex md:px-8">
           <div>
-            <div className="flex items-center justify-between pl-3 py-2 md:py-5 md:block bg-emerald-300">
+            <div className="flex items-center justify-between">
               {/* LOGO */}
               <Link href="/">
                 <Image
@@ -54,40 +54,40 @@ export default function Navbar() {
           </div>
           <div>
             <div
-              className={`flex-1 bg-emerald-300 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              className={`flex-1 justify-self-center md:block md:pb-0 md:mt-0 ${
                 navbar ? "md:p-0 block" : "hidden"
               }`}
             >
-              <ul className="h-screen md:h-auto bg-yellow-100 items-center justify-center text-xl">
-                <li className="mt-24 pb-6 text-4xl text-black font-bold py-6 md:text-4xl md:flex text-center border-b-2 border-black md:border-b-0 bg-emerald-300 ">
+              <ul className="lg:flex lg:flex-row lg:text-md lg:m-4 lg:gap-4 font-bold">
+                <li className="">
                   <Link href="/">HOME</Link>{" "}
                 </li>
-                <li className="pb-6 text-4xl text-black font-bold py-6 px-6 text-center  border-b-2 border-black md:border-b-0 bg-emerald-300 ">
+                <li className="">
                   <Link href="/developers" onClick={() => setNavbar(!navbar)}>
                     DEVELOPERS
                   </Link>
                 </li>
-                <li className="pb-6 text-4xl text-black font-bold py-6 px-6 text-center border-b-2 border-black md:border-b-0 bg-emerald-300">
+                <li className="">
                   <Link href="/charity" onClick={() => setNavbar(!navbar)}>
                     CHARITIES & NFPs
                   </Link>
                 </li>
-                <li className="pb-6 text-4xl text-black font-bold py-6 px-6 text-center border-b-2 border-black md:border-b-0 bg-emerald-300">
+                <li className="">
                   <Link href="/partners" onClick={() => setNavbar(!navbar)}>
                     OUR PARTNERS
                   </Link>
                 </li>
-                <li className="pb-6 text-4xl text-black font-bold py-6 px-6 text-center border-b-2 border-black md:border-b-0 bg-emerald-300 ">
+                <li className="">
                   <Link href="/testimonials" onClick={() => setNavbar(!navbar)}>
                     TESTIMONIALS
                   </Link>
                 </li>
-                <li className="pb-6 text-4xl text-black font-bold py-6 px-6 text-center border-b-2 border-black md:border-b-0 bg-emerald-300 ">
+                <li className="">
                   <Link href="/contact-us" onClick={() => setNavbar(!navbar)}>
                     CONTACT US
                   </Link>
                 </li>
-                <li className="pb-6 text-4xl text-black font-bold py-6 px-6 text-center md:border-b-0 bg-emerald-300">
+                <li className="">
                   <Link href="/about" onClick={() => setNavbar(!navbar)}>
                     ABOUT US
                   </Link>

d0ba7d6c5ace9ad6f894d6570595421f599f46fe
Haya Minhas <faithwaheed@icloud.com>
2023-12-13 19:00:25 +0000

    fix: wrong prop name

--- src/components/DevelopersAvailableProjectsList.jsx
+++ src/components/DevelopersAvailableProjectsList.jsx
@@ -1,10 +1,8 @@
 "use client";
-import FetchProjectsByStatus from "@/db-components/FetchProjectsByStatus";
 import DevelopersAvailableProject from "./DevelopersAvailableProject";
  
  // Return the line item for the developers active project
  export default async function  DevelopersAvailableProjectsList({projectsArray, regInterestInProjectsFunc}) {
-   let projectsArray = await FetchProjectsByStatus("3");
  
      return (
         <ol>

28f74435cbb173a8c521edb290851d2f01b29bcd
Haya Minhas <faithwaheed@icloud.com>
2023-12-13 17:35:05 +0000

    fix: convert postcss obj to es module

--- src/app/font/font.jsx
+++ src/app/font/font.jsx
@@ -1,14 +1,14 @@
-import { Domine, Arimo } from "next/font/google";
+import {Domine, Arimo} from 'next/font/google'
 
 export const domine = Domine({
-  variants: ["400", "700"],
+  weight: ["400", "700"],
   subsets: ["latin-ext"],
   display: "swap",
   variable: "--domine-font",
 });
 
 export const arimo = Arimo({
-  variants: ["400", "700"],
+  weight: ["400", "700"],
   subsets: ["latin-ext"],
   display: "swap",
   variable: "--arimo-font",

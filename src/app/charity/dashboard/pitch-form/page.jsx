a7cfa465a10ec329539ef6409e8bda062d366a39
Haya Minhas <faithwaheed@icloud.com>
2023-12-14 14:47:38 +0000

    styling of pitch form, contact form, charity reg form, dev reg form

--- src/app/charity/dashboard/pitch-form/page.jsx
+++ src/app/charity/dashboard/pitch-form/page.jsx
@@ -1,17 +1,17 @@
 "use client";
-import Navbar from "@/components/navbar";
+
 import Link from "next/link";
-import Footer from "@/components/Footer";
+
 import PitchForm from "@/components/pitchForm";
 
 export default function PitchRegistration() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+
       <div className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:flex-grow">
         <PitchForm />
       </div>
-      <Footer />
+
     </div>
   );
 }
--- src/app/developers/register/page.jsx
+++ src/app/developers/register/page.jsx
@@ -1,5 +1,3 @@
-import Navbar from "@/components/navbar";
-import Footer from "@/components/Footer";
 
 import "../../../../globals.css"
 import Link from "next/link";
@@ -7,12 +5,10 @@ import DeveloperRegistration from "@/components/developerRegistration";
 
 export default function DeveloperRegister() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
+    <div className="flex flex-col">
       <div className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:flex-grow">
         <DeveloperRegistration />
       </div>
-      <Footer />
     </div>
   );
 }

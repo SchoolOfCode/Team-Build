3df14cd8794e61daafaeb148843a39b5b5b556fa
Yogiz88 <m.z.zawadzkimichal@gmail.com>
2023-12-15 11:26:15 +0000

    dashboard

--- src/components/DevelopersActiveProject.jsx
+++ src/components/DevelopersActiveProject.jsx
@@ -1,12 +1,11 @@
-
-
-export default function  DevelopersActiveProject({project}) {
-
-   // Return the line item for the developers active project
-   return (
-     <li>
-      <p>{project.title}</p>
+export default function DevelopersActiveProject({ project }) {
+  // Return the line item for the developers active project
+  return (
+    <li className="grid grid-cols-3">
+      <h1>{project.title}</h1>
       <p>{project.short_desc}</p>
-      </li>
-   );
+      <button>Details</button>
+      <span className="border-b border-black"></span>
+    </li>
+  );
 }

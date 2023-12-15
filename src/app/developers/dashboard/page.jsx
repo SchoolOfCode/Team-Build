fa2a633df26ff5e1b8c4fde2d8109fe32b9f1fff
Yogiz88 <m.z.zawadzkimichal@gmail.com>
2023-12-15 10:22:30 +0000

    developers dashboard redesign

--- src/app/developers/dashboard/page.jsx
+++ src/app/developers/dashboard/page.jsx
@@ -3,16 +3,13 @@ import DisplayDevelopersDashboard2 from "@/components/DisplayDevelopersDashboard
 
 export default function DeveloperDasboard() {
   return (
-    <div>
-      <h2 className="subTitle text-center text-4xl font-bold p-2 bg-emerald-300">
-        Dashboard
-      </h2>
+    <div className="text-left lg:m-auto lg:p-10 lg:h-[8/12] lg:flex lg:bg-slate-50 lg:justify-center lg:flex-col lg:align-middle lg:text-xl lg:w-10/12 lg:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] lg:rounded-md">
       <DisplayDevelopersDashboard2 />
-      <div className="flex flex-col items-center pt-10">
-        <button className="button m-3 w-60">
+      <div className="flex flex-row lg:flex-row justify-center lg:justify-center lg:gap-20 items-center pt-10">
+        <button className="button border-4 text-red-400 border-red-400 font-bold rounded w-44 h-10 text-sm lg:w-64 lg:text-md lg:mt-4 m-2">
           <Link href="./dashboard/how-voting-works">How does voting work?</Link>
         </button>
-        <button className="button m-3 w-60">
+        <button className="button bg-red-400  font-bold rounded w-44 h-10 text-sm lg:w-64 lg:text-md lg:mt-4 m-2">
           <Link href="./dashboard/voting">Votes Table</Link>
         </button>
       </div>

bdc3708050dbc8f7de31978cd0f958f580672109
Yogiz88 <m.z.zawadzkimichal@gmail.com>
2023-12-15 11:52:37 +0000

    devs dashboard

--- src/components/DevelopersActiveProject.jsx
+++ src/components/DevelopersActiveProject.jsx
@@ -1,11 +1,12 @@
 export default function DevelopersActiveProject({ project }) {
   // Return the line item for the developers active project
   return (
-    <li className="grid grid-cols-3">
-      <h1>{project.title}</h1>
-      <p>{project.short_desc}</p>
-      <button>Details</button>
-      <span className="border-b border-black"></span>
+    <li className="lg:grid lg:grid-cols-6 lg:border-b lg:border-slate-400 lg:mr-20 border-b border-slate-700 pb-4">
+      <h1 className="col-span-2">{project.title}</h1>
+      <p className="col-span-3 flex flex-wrap">{project.short_desc}</p>
+      <button className="col-span-1 border-4 border-emerald-500 w-20 lg:w-10/12 text-center text-emerald-500 font-bold justify-self-end">
+        Details
+      </button>
     </li>
   );
 }
--- src/components/DevelopersAvailableProject.jsx
+++ src/components/DevelopersAvailableProject.jsx
@@ -1,15 +1,19 @@
 // Return the line item for the developers active project
-export default function  DevelopersAvailableProject({project, regInterestInProjectFunc}) {
+export default function DevelopersAvailableProject({
+  project,
+  regInterestInProjectFunc,
+}) {
+  function handleClick() {
+    return regInterestInProjectFunc(project.project_id);
+  }
 
-   function handleClick() {
-      return regInterestInProjectFunc(project.project_id);
-    }
-   
-       return (
-         <li>
-          <p>{project.title}</p>
-          <p>{project.short_desc}</p>
-          <button onClick={handleClick}>I am interested</button> 
-          </li>
-       );
-    }
+  return (
+    <li className="lg:grid lg:grid-cols-6 lg:border-b lg:border-slate-400 lg:mr-20 border-b border-slate-700 pb-4">
+      <h1 className="col-span-2">{project.title}</h1>
+      <p className="col-span-3 flex flex-wrap">{project.short_desc}</p>
+      <button className="col-span-1 border-4 border-emerald-500 w-20 lg:w-10/12 text-center text-emerald-500 font-bold justify-self-end">
+        Details
+      </button>
+    </li>
+  );
+}

ebcfc75cbcfe66e5302739d77afd26bc2c238149
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2023-12-18 12:16:13 +0000

    error with displayig data

--- src/components/DisplayProjectsForVoting.jsx
+++ src/components/DisplayProjectsForVoting.jsx
@@ -6,29 +6,28 @@ import ProjectVotingCard from "./ProjectVotingCard";
 export default function DisplayProjectsForVoting() {
   // set State on projects that are available for voting
   const [projectsForVoting, setProjectsForVoting] = useState([]);
-  
-  
-  
-   // Display the initial list of projects that are available to be voted on.
-   // These are projects with a status of 3
-  useEffect(() => {
-     FetchProjectsByStatus(3).then((projectsForVoting) => {
-       setProjectsForVoting(projectsForVoting);})
-     }, []);
 
-  
+  // Display the initial list of projects that are available to be voted on.
+  // These are projects with a status of 3
+  useEffect(() => {
+    FetchProjectsByStatus(3).then((data) => {
+      setProjectsForVoting(projectsForVoting);
+    });
+  }, []);
+
   return (
     <>
-     <div className="lg:grid section flex flex-col justify-top items-left rounded-lg p-2 m-2  h-40 mt-4">
-        <h1 className="subTitle text-xl font-bold">Vote for your favourite projects</h1>
+      <div className="lg:grid section flex flex-col justify-top items-left rounded-lg p-2 m-2  h-40 mt-4">
+        <h1 className="subTitle text-xl font-bold">
+          Vote for your favourite projects
+        </h1>
         <ol>
           {projectsForVoting.map((projectForVoting) => {
+            console.log(projectForVoting);
             return (
               <ProjectVotingCard
                 key={projectForVoting.project_id}
                 project={projectForVoting}
-                functionToVoteOneThumb={functionToVoteOneThumb}
-                functionToVoteTwoThumbs={functionToVoteTwoThumbs}
               />
             );
           })}
--- src/components/ProjectVotingCard.jsx
+++ src/components/ProjectVotingCard.jsx
@@ -1,53 +1,54 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import DeveloperDashboard from '../developers/dashboard/page';
 //import Developers from '../developers/page';
-import InsertVote from '../db-components/InsertVote';
-import Link from 'next/link';
-
+import InsertVote from "../db-components/InsertVote";
+import Link from "next/link";
 
 // Return the project voting card
-export default function  ProjectVotingCard({params}) {
-  
+export default function ProjectVotingCard({ params }) {
   const [isVotedFor, setIsVotedFor] = useState(false);
-  
-  const project = params.project
-   
-  //Function that is invoked when the upvote 1 button is clicked. This inserts a votes intersection table, 
-  //and adds one to the total_score for the project using a database trigger function.
-function handleClickOne() {
-  const devId = localStorage.getItem("userId");
-  InsertVote(devId, project.project_id, 1 ).then(()=> 
-  setIsVotedFor(isVotedFor = true))
-  return (console.log("click One",isVotedFor));
-};
 
- //Function that is invoked when the upvote 2 button is clicked. This inserts a votes intersection table, 
+  console.log(params);
+
+  // const project = params.project;
+
+  //Function that is invoked when the upvote 1 button is clicked. This inserts a votes intersection table,
+  //and adds one to the total_score for the project using a database trigger function.
+  function handleClickOne() {
+    const devId = localStorage.getItem("userId");
+    InsertVote(devId, project.project_id, 1).then(() =>
+      setIsVotedFor(!isVotedFor)
+    );
+    return console.log("click One", isVotedFor);
+  }
+
+  //Function that is invoked when the upvote 2 button is clicked. This inserts a votes intersection table,
   //and adds two to the total_score for the project using a database trigger function.
   function handleClickTwo() {
     const devId = localStorage.getItem("userId");
-    InsertVote(devId, project.project_id, 2 ).then(()=> 
-    setIsVotedFor(isVotedFor = true))
-    return (console.log("click Two",isVotedFor));
-  };
+    InsertVote(devId, project.project_id, 2).then(() =>
+      setIsVotedFor(!isVotedFor)
+    );
+    return console.log("click Two", isVotedFor);
+  }
 
-   return (
-    
-<div>
-    <Link key={project.project_id} href={`../dashboard/voting/${project.project_id}`}>
-    <div className='projectDiv'>
-
-     <li>
-      <p>{project.title}</p>
-      <p>{project.short_desc}</p>
-      </li>
-      </div>
+  return (
+    <div>
+      <Link
+        key={project.project_id}
+        href={`../dashboard/voting/${project.project_id}`}
+      >
+        <div className="projectDiv">
+          <li>
+            <p>{project.title}</p>
+            <p>{project.short_desc}</p>
+          </li>
+        </div>
       </Link>
       <button onClick={handleClickOne}>Vote Up</button>
       <button onClick={handleClickTwo}>Vote Up More</button>
     </div>
- 
-
   );
 }

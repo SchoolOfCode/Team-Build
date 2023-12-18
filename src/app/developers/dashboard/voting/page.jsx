03508b8b69ecb87b0a2d1af7fef328c575a69c85
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2023-12-18 15:08:49 +0000

    basic styling for voting page

--- src/app/developers/dashboard/voting/page.jsx
+++ src/app/developers/dashboard/voting/page.jsx
@@ -1,28 +1,35 @@
-
-
-import DisplayProjectsForVoting from '@/components/DisplayProjectsForVoting';
+import DisplayProjectsForVoting from "@/components/DisplayProjectsForVoting";
 
 //clear the cache of projects fetched every 5 seconds?
 //export const revalidate = 5;
 
-export default function VotingPage(){
-  
+export default function VotingPage() {
   return (
-    <div>
-     <DisplayProjectsForVoting  />;
-       
+    <div className="lg:mt-[10px] text-left lg:m-auto lg:h-screen lg:bg-slate-50 lg:flex lg:flex-col lg:text-xl lg:w-10/12 lg:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] lg:rounded-md">
+      <div className="flex flex-col justify-center">
+        <h1 className="m-4 leading-snug text-3xl font-bold lg:text-5xl lg:leading-snug">
+          Cast your{" "}
+          <span className="bg-emerald-400 py-1 px-2 rounded-md mt-2">
+            Votes
+          </span>
+        </h1>
+        <span className="border-b border-black w-11/12 ml-2 mr-2"></span>
+      </div>
+      <DisplayProjectsForVoting />;
     </div>
   );
-  }
+}
+
+/*
 
 
-
-
-
-
-
-
-
-
-
-
+      <div className="flex flex-row lg:flex-row justify-center lg:justify-center lg:gap-20 items-center mb-4">
+        <button className="button border-4 text-red-400 border-red-400 font-bold rounded w-44 h-10 text-sm lg:w-64 lg:text-md lg:mt-4 m-2">
+          <Link href="./dashboard/how-voting-works">How does voting work?</Link>
+        </button>
+        <button className="button bg-red-400  font-bold rounded w-44 h-10 text-sm lg:w-64 lg:text-md lg:mt-4 m-2">
+          <Link href="./dashboard/voting">Votes Table</Link>
+        </button>
+      </div>
+   
+*/
--- src/components/ProjectVotingCard.jsx
+++ src/components/ProjectVotingCard.jsx
@@ -6,42 +6,59 @@
 import Link from "next/link";
 
 // Return the project voting card
-export default function ProjectVotingCard( params ) {
- // const [isVotedFor, setIsVotedFor] = useState(false);
+export default function ProjectVotingCard(params) {
+  // const [isVotedFor, setIsVotedFor] = useState(false);
 
   console.log(params);
 
-  const project = params.project
+  const project = params.project;
   const functionToVoteOne = params.functionToVoteOne;
   const functionToVoteTwo = params.functionToVoteTwo;
 
   //Function that is invoked when the upvote 1 button is clicked. This inserts a votes intersection table,
   //and adds one to the total_score for the project using a database trigger function.
   function handleClickOne() {
-  return functionToVoteOne(project.project_id);
-   }
+    return functionToVoteOne(project.project_id);
+  }
 
   //Function that is invoked when the upvote 2 button is clicked. This inserts a votes intersection table,
   //and adds two to the total_score for the project using a database trigger function.
   function handleClickTwo() {
-         return functionToVoteTwo(project.project_id);
-       }
+    return functionToVoteTwo(project.project_id);
+  }
 
   return (
     <div>
       <Link
         key={project.project_id}
         href={`../dashboard/voting/${project.project_id}`}
-      >
-        <div className="projectDiv">
-          <li>
-            <p>{project.title}</p>
-            <p>{project.short_desc}</p>
-          </li>
-        </div>
-      </Link>
-      <button onClick={handleClickOne}>Vote Up</button>
-      <button onClick={handleClickTwo}>Vote Up More</button>
+      ></Link>
+      <li className="lg:grid lg:grid-cols-4 lg:border-b lg:border-slate-400 lg:w-full border-b border-slate-700 pb-4">
+        <h1 className="col-span-1">{project.title}</h1>
+        <p className="col-span-2">{project.short_desc}</p>
+        <span className="col-span-1 lg:flex lg:flex-row lg:gap-4 flex justify-evenly">
+          <button onClick={handleClickOne} className="">
+            Vote Up
+          </button>
+          <button onClick={handleClickTwo} className="">
+            Vote Up More
+          </button>
+        </span>
+      </li>
     </div>
   );
 }
+
+/*
+<li className="lg:grid lg:grid-cols-6 lg:border-b lg:border-slate-400 lg:mr-20 border-b border-slate-700 pb-4">
+       <Link
+        key={project.project_id}
+        href={`../dashboard/voting/${project.project_id}`}
+      >
+        <h1 className="col-span-2">{project.title}</h1>
+        <p className="col-span-3 flex flex-wrap">{project.short_desc}</p>
+        </Link>
+        <button onClick={handleClickOne} className="button bg-red-400 font-bold rounded w-44 h-16 text-xl lg:w-56 lg:text-2xl lg:mt-4 mt-10>Vote Up</button>
+        <button onClick={handleClickTwo} className="button bg-red-400 font-bold rounded w-44 h-16 text-xl lg:w-56 lg:text-2xl lg:mt-4 mt-10>Vote Up More</button>
+        </li>
+*/

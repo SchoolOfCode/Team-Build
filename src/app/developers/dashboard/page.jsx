2a2242c763a65906e867289c78339e813386f052
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2023-12-19 14:51:43 +0000

    dev dashboard styling how to vote styling project detail styling

--- src/app/developers/dashboard/page.jsx
+++ src/app/developers/dashboard/page.jsx
@@ -3,7 +3,7 @@ import DisplayDevelopersDashboard2 from "@/components/DisplayDevelopersDashboard
 
 export default function DeveloperDashboard() {
   return (
-    <div className="lg:mt-[10px] text-left lg:m-auto lg:h-[8/12] lg:bg-slate-50 lg:flex lg:justify-center lg:flex-col lg:align-middle lg:text-xl lg:w-10/12 lg:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] lg:rounded-md">
+    <div className="lg:lg:overflow-auto lg:mt-[10px] text-left lg:m-auto lg:h-[8/12] lg:bg-slate-50 lg:flex lg:justify-center lg:flex-col lg:align-middle lg:text-xl lg:w-10/12 lg:shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] lg:rounded-md">
       <div className="flex flex-col justify-center">
         <h1 className="m-4 leading-snug text-3xl font-bold lg:text-5xl lg:leading-snug">
           Developers{" "}
@@ -19,7 +19,7 @@ export default function DeveloperDashboard() {
           <Link href="./dashboard/how-voting-works">How does voting work?</Link>
         </button>
         <button className="button bg-red-400  font-bold rounded w-44 h-10 text-sm lg:w-64 lg:text-md lg:mt-4 m-2">
-          <Link href="./dashboard/voting">Votes Table</Link>
+          <Link href="./dashboard/voting">Vote Up New Projects</Link>
         </button>
       </div>
     </div>

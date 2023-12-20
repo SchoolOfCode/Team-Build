3409e74b1f75ea964d64fa968c295270a38d2c93
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2023-12-20 10:44:39 +0000

    pitch form

--- src/components/pitchForm.jsx
+++ src/components/pitchForm.jsx
@@ -106,64 +106,63 @@ export default function PitchForm() {
           </div>
         ) : (
           <div>
-          <form
-            className="bg-slate-50 mt-5 tracking-wider flex flex-col w-full lg:grid lg:grid-cols-2 lg:gap-10 gap-5"
-            method="POST"
-            action="https://team-5-final-project-pi.vercel.app/developer/register"
-            onSubmit={submitReg}
-          >
-            {/* div for title */}
-            <div>
-              {/* <label className="appearance-none border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none">
+            <form
+              className="bg-slate-50 mt-5 tracking-wider flex flex-col w-full lg:flex lg:flex-row justify-evenly lg:gap-10 gap-5"
+              method="POST"
+              action="https://team-5-final-project-pi.vercel.app/developer/register"
+              onSubmit={submitReg}
+            >
+              {/* div for title */}
+              <div className="flex flex-col">
+                <div>
+                  {/* <label className="appearance-none border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none">
                 Title:{" "}
               </label> */}
-              <input
-                type="text"
-                name="Project_Title"
-                onChange={handleInput}
-                value={registration.Project_Title}
-                //className= "appearance-none bg-transparent border border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-8 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
-              className="appearance-none bg-transparent border-b pb-2 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
-                 placeholder="Project Title:"
-                required
-              />
-            </div>
+                  <input
+                    type="text"
+                    name="Project_Title"
+                    onChange={handleInput}
+                    value={registration.Project_Title}
+                    //className= "appearance-none bg-transparent border border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-8 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    className="appearance-none bg-transparent border-b pb-2 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    placeholder="Project Title:"
+                    required
+                  />
+                </div>
 
-            {/* div for Short Description */}
-            <div>
-              {/* <label className="appearance-none border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none">
+                {/* div for Short Description */}
+                <div>
+                  {/* <label className="appearance-none border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none">
                 Short Description:{" "}
               </label> */}
-              <textarea
-                type="text"
-                name="Short_Descr"
-                onChange={handleInput}
-                value={registration.Short_Descr}
-                className="appearance-none bg-transparent border-b pb-2 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
-                // className="appearance-none bg-transparent border border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-35 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
-                 placeholder="Brief Summary:"
-                required
-              />
-            </div>
+                  <textarea
+                    type="text"
+                    name="Short_Descr"
+                    onChange={handleInput}
+                    value={registration.Short_Descr}
+                    className="appearance-none bg-transparent border-b pb-2 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    // className="appearance-none bg-transparent border border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-35 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    placeholder="Brief Summary:"
+                    required
+                  />
+                </div>
 
-           
-
-            {/* div for Video link */}
-            <div>
-              {/* <label className="appearance-none border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none">
+                {/* div for Video link */}
+                <div>
+                  {/* <label className="appearance-none border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none">
                 YouTube Video Link:{" "}
               </label> */}
-              <input
-                name="Video_Link"
-                onChange={handleInput}
-                value={registration.Video_Link}
-                //className="appearance-none bg-transparent border border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-8 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
-                className="appearance-none bg-transparent border-b pb-2 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-gray-600 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                placeholder="YouTube Video Link:"
-              />
-            </div>
-
-            {/* <div className=" py-2 mr-1">
+                  <input
+                    name="Video_Link"
+                    onChange={handleInput}
+                    value={registration.Video_Link}
+                    //className="appearance-none bg-transparent border border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-8 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    className="appearance-none bg-transparent border-b pb-2 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-gray-600 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    placeholder="YouTube Video Link:"
+                  />
+                </div>
+              </div>
+              {/* <div className=" py-2 mr-1">
               <label className="appearance-none bg-transparent border-b font-bold pb-2 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full text-black mr-3 py-1 px-2 leading-tight focus:outline-none">
                 <Link href="/termsandconditions">Terms & Conditions:</Link>
               </label>
@@ -176,37 +175,40 @@ export default function PitchForm() {
                 required
               />
             </div> */}
+              <div className="flex flex-col">
+                {/* div for Long Description */}
+                <div className="lg:row-start-1 lg:row-end-4 ">
+                  <label className="appearance-none border-gray-600 placeholder:text-xl w-full text-gray-600 mr-3 py-1 px-2 leading-tight focus:outline-none">
+                    Detailed Description:
+                  </label>
+                  <textarea
+                    type="VARCHAR"
+                    name="Long_Descr"
+                    onChange={handleInput}
+                    value={registration.Long_Descr}
+                    className="appearance-none bg-grey-200 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-48 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
+                    required
+                  ></textarea>
+                </div>
 
-            {/* div for submit button link */}
-            <div>
-            <button
-              type="submit"
-              className="w-full bg-red-400 text-white py-2 rounded-md">
-              Submit Form
-            </button>
-            </div>
-
-             {/* div for Long Description */}
-             <div className="lg:row-start-1 lg:row-end-4 ">
-              <label className="appearance-none border-gray-600 placeholder:text-xl w-full text-gray-600 mr-3 py-1 px-2 leading-tight focus:outline-none">
-                Detailed Description:
-              </label>
-              <textarea
-                type="VARCHAR"
-                name="Long_Descr"
-                onChange={handleInput}
-                value={registration.Long_Descr}
-                className="appearance-none bg-grey-200 border-gray-600 placeholder:text-gray-600 placeholder:text-xl w-full h-48 text-black mr-3 py-1 px-2 leading-tight focus:outline-none"
-                required
-              ></textarea>
-            </div>
-
-            {!isValid && (
-              <div className="text-red-500">
-                Please fill in all required fields.
+                {/* div for submit button link */}
+                <div>
+                  <button
+                    type="submit"
+                    className="w-full bg-red-400 text-white py-2 rounded-md"
+                  >
+                    Submit Form
+                  </button>
+                </div>
               </div>
-            )}
-          </form></div>)}
+              {!isValid && (
+                <div className="text-red-500">
+                  Please fill in all required fields.
+                </div>
+              )}
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );

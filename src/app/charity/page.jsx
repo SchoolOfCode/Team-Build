dec66621c3943e496d47b5e9d4524c3f108c16ed
davidjohnd <99533641+davidjohnd@users.noreply.github.com>
2023-12-20 12:10:33 +0000

    how it works styling added but button still wrong place desktop

--- src/app/charity/page.jsx
+++ src/app/charity/page.jsx
@@ -15,17 +15,19 @@ export default function Charity() {
     <div className="grid lg:grid-cols-2 grid-cols-1">
           <div className="grid col-span-1 lg:mx-40 ml-6">
         
-        <div className="mt-10 flex flex-col justify-between lg:flex lg:flex-row-reverse lg:align-bottom lg:w-4/5">
-          <p className="text-4xl font-bold mb-10 lg:max-w-2xl lg:mr-24 lg:leading-relaxed">
+        <div className="mt-10 justify-between lg:flex  lg:align-bottom lg:w-4/5 lg:leading-relaxed lg:items-start">
+          <p className="text-4xl font-bold mb-10 lg:max-w-2xl lg:mr-24 lg:leading-relaxed mr-6">
             Building Websites, Transforming Lives with Every Line of Code
           </p> 
-                    <button className="button mr-20 bg-red-400 font-bold rounded w-44 h-16 text-xl lg:w-56 lg:text-2xl lg:mt-4 mt-10">
+          <div className="flex">
+                    <button className="button mr-20 bg-red-400 font-bold rounded w-44 h-16 text-xl lg:w-56 lg:text-2xl lg:mt-4 mt-2 ml-14">
             <Link href={"/charity/register"}>Join us</Link>{" "}
 
           </button>
         </div>
         </div>
-        <div className="grid col-span-1 mt-10">
+        </div>
+        <div className="grid col-span-1 mt-10 mr-6">
           <Image
       src="/howitworksv2.svg"
       width={736}

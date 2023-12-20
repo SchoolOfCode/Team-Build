baa213618e87c7120c7c953326c932f7ef7d2949
wendyrich63 <142948201+wendyrich63@users.noreply.github.com>
2023-12-20 10:28:27 +0000

    re-ordering and maintaining the message working

--- src/components/DisplayProjectsForVoting.jsx
+++ src/components/DisplayProjectsForVoting.jsx
@@ -9,60 +9,21 @@ export default function DisplayProjectsForVoting() {
   // set State on projects that are available for voting
   const [projectsForVoting, setProjectsForVoting] = useState([]);
   const [isVotedFor, setIsVotedFor] = useState(false);
-  const [thankYouMsg, setThankYouMsg] = useState(false);
-
-  // Display the initial list of projects that are available to be voted on.
-  // These are projects with a status of 3, that this developer has not already voted for.
-  // useEffect(() => {
-  //   FetchProjectsByStatus(3).then((data) => setProjectsForVoting(data));
-  // }, [isVotedFor]);
-
-  // Display the initial list of projects that are available to be voted on.
-  // These are projects with a status of 3, that this developer has not already voted for.
-  // useEffect(() => {
-  //   // First fetch all the votes that this developer has
-  //   const devsId = localStorage.getItem("userId");
-  //   FetchVotesByDevId(devsId).then((data) => {
-  //     const votesArray = data;
-
-  //     //Now build from the array of Votes above a simple array of project_id's
-  //     const projectIdArray = votesArray.map((entry) => entry.project_id);
-
-  //     // Then fetch all projects with a status of 3 (awaiting voting) and filter out those
-  //     // that this developer has already voted for i.e.already exist in the projectIdArray
-  //     FetchProjectsByStatus(3).then((data) => {
-  //     //   const filteredProjects = data.filter(
-  //     //     (entry) => !projectIdArray.includes(entry.project_id)
-  //     //   );
 
 
-  //     const filteredProjects = data.map((entry) => {
-  //        if (projectIdArray.includes(entry)) {
-  //         const newEntry = {...entry, msg: true}
-  //         return;
-  //         } else {
-  //           const newEntry = {...entry, msg: false}
-  //         }
-  //       }
-  //     });
-  //     setProjectsForVoting(filteredProjects);
-  //     console.log(filteredProjects);
-  //   })
-
-  //   });
-  // }, [isVotedFor]);
 
   useEffect(() => {
     // First fetch all the votes that this developer has
     const devsId = localStorage.getItem("userId");
     FetchVotesByDevId(devsId).then((data) => {
       const votesArray = data;
-  
+
       // Now build from the array of Votes above a simple array of project_id's
       const projectIdArray = votesArray.map((entry) => entry.project_id);
-  
-      // Then fetch all projects with a status of 3 (awaiting voting) and filter out those
+
+      // Then fetch all projects with a status of 3 (awaiting voting) and for those
       // that this developer has already voted for i.e. already exist in the projectIdArray
+      // set a new flag called "msg" to true, so this can be used to display the thank you msg
       FetchProjectsByStatus(3).then((data) => {
         const filteredProjects = data.map((entry) => {
           if (projectIdArray.includes(entry.project_id)) {
@@ -73,7 +34,7 @@ export default function DisplayProjectsForVoting() {
             return newEntry;
           }
         });
-  
+
         setProjectsForVoting(filteredProjects);
         console.log(filteredProjects);
       });
@@ -87,7 +48,7 @@ export default function DisplayProjectsForVoting() {
   //and adds one to the total_score for the project using a database trigger function.
   function functionToVoteOne(project_id) {
     const devsId = localStorage.getItem("userId");
-    InsertVote(devsId, project_id, 1).then(() => setIsVotedFor(isVotedFor));
+    InsertVote(devsId, project_id, 1).then(() => setIsVotedFor(!isVotedFor));
   }
 
   //Function that is invoked when the upvote 2 button is clicked. This inserts a votes intersection table,

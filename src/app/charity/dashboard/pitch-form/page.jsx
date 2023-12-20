1c4ce20bc4a8f73330631da6ddd36d553495e467
Haya Minhas <faithwaheed@icloud.com>
2023-12-20 11:59:28 +0000

    done

--- src/app/charity/dashboard/pitch-form/page.jsx
+++ src/app/charity/dashboard/pitch-form/page.jsx
@@ -5,8 +5,8 @@ import PitchForm from "@/components/pitchForm";
 export default function PitchRegistration() {
   
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:flex-grow">
+    <div className="flex flex-col">
+    <div className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:flex-grow">
         <PitchForm />
       </div>
     </div>
